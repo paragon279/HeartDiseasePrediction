df32d92ad75f574c1698f822bc41fb3a48677b32
c0r2a-lab <richardjkennedy9699@gmail.com>
2024-01-30 00:46:06 +0800

    update: understood acc

--- HeartDiseasePrediction/HeartDiseasePrediction.py
+++ HeartDiseasePrediction/HeartDiseasePrediction.py
@@ -24,3 +24,43 @@ print(disease_df.TenYearCHD.value_counts())
 plt.figure(figsize=(7,5))
 sn.countplot(x='TenYearCHD', data=disease_df, palette="BuGn_r")
 plt.show()
+
+laste = disease_df['TenYearCHD'].plot()
+plt.show()
+
+# Training and Testing sets
+# Split data, normalization of dataset
+X = np.asarray(disease_df[['age', 'Sex_male', 'cigsPerDay', 'totChol', 'sysBP', 'glucose']])
+y = np.asarray(disease_df['TenYearCHD'])
+
+# normalization of the dataset
+X = preprocessing.StandardScaler().fit(X).transform(X)
+
+# Train and test  , split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.3, random_state=4)
+
+print('Train set:', X_train.shape, y_train.shape)
+print('Test set:', X_test.shape, y_test.shape)
+
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+
+# Evaluation and accuracy
+from sklearn.metrics import jaccard_score
+
+print('')
+print('Accuracy of the model in jaccard similarity score is = ',
+      jaccard_score(y_test, y_pred))
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+
+score = rf.score(X_test,  y_test)*100
+print('Accuracy of the model is = ', score)
