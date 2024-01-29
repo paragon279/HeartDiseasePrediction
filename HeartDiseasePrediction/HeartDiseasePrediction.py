4aeb1ff734748eab74ec9d931f3572689e73aaa2
c0r2a-lab <richardjkennedy9699@gmail.com>
2024-01-29 21:49:34 +0800

    update: understood pyplot.figure

--- HeartDiseasePrediction/HeartDiseasePrediction.py
+++ HeartDiseasePrediction/HeartDiseasePrediction.py
@@ -19,3 +19,8 @@ disease_df.rename(columns = {'male':'Sex_male'}, inplace = True)
 disease_df.dropna(axis = 0, inplace = True)
 print(disease_df.head(), disease_df.shape)
 print(disease_df.TenYearCHD.value_counts())
+
+# counting number of patients affected with CHD
+plt.figure(figsize=(7,5))
+sn.countplot(x='TenYearCHD', data=disease_df, palette="BuGn_r")
+plt.show()
